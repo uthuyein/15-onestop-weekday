f0684eebf657fcc683c9a1b5d3e1a0c4630c1af2
uthuyein <uthuyeinlinn@gmail.com>
2025-12-12 11:05:27 +0630

    update synchronized

---
+++ 05-hibernate-jpa/05-synchronized/src/main/resources/META-INF/import.sql
@@ -0,0 +1,2 @@
+insert into account(name,balance)values('John',50000.00);
+insert into account(name,balance)values('Patrick',50000.00);
---
+++ 05-hibernate-jpa/05-synchronized/src/test/java/com/jdc/mkt/test/SynchronizedTest.java
@@ -0,0 +1,88 @@
+package com.jdc.mkt.test;
+
+import org.junit.jupiter.api.Test;
+
+import com.jdc.mkt.entity.Account;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class SynchronizedTest {
+
+	EntityManagerFactory emf;
+
+	@Test
+	void test() {
+		emf = Persistence.createEntityManagerFactory("synchronized");
+		Thread operationOne = threadOne(5000.00);
+		Thread operationTwo = threadTwo(5000.00);
+
+		operationOne.start();
+		operationTwo.start();
+
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Thread threadOne(double amount) {
+		return new Thread(() -> {
+			var em = emf.createEntityManager();
+			var account = em.find(Account.class, 1);
+			
+			try {
+				em.getTransaction().begin();
+
+				System.out.println("=============     Before  Update Operation One     =========");
+				System.out.println("=============     Balance(::: Ope One :::)" + account.getBalance() + "     =========");
+
+				Thread.sleep(800);
+
+				em.refresh(account);
+				account.setBalance(account.getBalance() - amount);
+				em.flush();
+
+				System.out.println("=============     After  Update Operation One     =========");
+				System.out.println("=============     Balance(::: Ope One :::)" + account.getBalance() + "     =========");
+
+				em.getTransaction().commit();
+				em.close();
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+	private Thread threadTwo(double amount) {
+		return new Thread(() -> {
+			var em = emf.createEntityManager();
+			var account = em.find(Account.class, 1);
+			
+			try {
+				em.getTransaction().begin();
+
+				System.out.println("=============     Before  Update Operation Two     =========");
+				System.out.println("=============     Balance(::: Ope Two :::)" + account.getBalance() + "     =========");
+
+				Thread.sleep(500);
+
+				em.refresh(account);
+				account.setBalance(account.getBalance() + amount);
+				em.flush();
+
+				System.out.println("=============     After  Update Operation Two     =========");
+				System.out.println("=============     Balance(::: Ope Two :::)" + account.getBalance() + "     =========");
+
+				em.getTransaction().commit();
+				em.close();
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+
+}
